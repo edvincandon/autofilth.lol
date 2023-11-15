4945b17fba0bb0db7ac9e64623ed8dc8bc3d332a
Edvin CANDON <edvin.candon@proton.ch>
2023-11-15 16:12:30 +0100

    Dark mode for carlos and dom

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} dark`}>
+    <html lang="en" className="dark">
+      <body className={`${inter.className} `}>
         <Link href="/">
           <div className="mt-20 relative flex flex-col place-items-center after:absolute after:inset-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:-z-20 after:h-[80px] after:w-[260px] after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 z-[-1]">
             <kbd>autofilth.lol</kbd>
